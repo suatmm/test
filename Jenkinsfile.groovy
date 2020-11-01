fb4cf92f2f5de43b975f456ec88faffb70090a79
Ilya Solovyev <ilya.solovyev@hotmail.com>
2020-11-01 22:11:40 +0300

    pipeline changes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,97 +1,101 @@
-String getChangedFilesList() {
-  changedFiles = []
-  for (changeLogSet in currentBuild.changeSets) { 
-      for (entry in changeLogSet.getItems()) { // for each commit in the detected changes
-          for (file in entry.getAffectedFiles()) {
-              changedFiles.add(file.getPath()) // add changed file to list
-          }
+library 'jenkins-library@DAP-1517_terraform_mvp'
+
+podTemplate(yaml: libraryResource("jenkins-agents/terraform.yaml")) {
+  node(POD_LABEL) {
+    
+    properties([
+      buildDiscarder(logRotator(numToKeepStr: "3")),
+      disableConcurrentBuilds(),
+      parameters([
+        booleanParam(
+          name: 'APPLY',
+          defaultValue: false,
+          description: 'Apply'),
+        booleanParam(
+          name: 'PLAN',
+          defaultValue: false,
+          description: 'PLAN')
+      ])
+    ])
+
+    stage('Terraform PLAN AND APPLY') {
+      container(name:'terraform', shell: "/bin/bash") {
+        def scmVars = checkout(scm)
+        // templateToProcessPaths = getChangedTemplateFiles()
+        println scmVars
+
+        // withEnv([
+        //   "OS_DOMAIN_NAME=${utils.getVaultSecret('secret/infra/selectel/terraform', 'OS_DOMAIN_NAME', 'terraform')}",
+        //   "OS_PASSWORD=${utils.getVaultSecret('secret/infra/selectel/terraform', 'OS_PASSWORD', 'terraform')}",
+        //   "OS_REGION_NAME=${utils.getVaultSecret('secret/infra/selectel/terraform', 'OS_REGION_NAME', 'terraform')}",
+        //   "OS_USERNAME=${utils.getVaultSecret('secret/infra/selectel/terraform', 'OS_USERNAME', 'terraform')}",
+        //   "OS_TENANT_ID=${utils.getVaultSecret('secret/infra/selectel/terraform', 'OS_TENANT_ID', 'terraform')}",
+        //   "SEL_TOKEN=${utils.getVaultSecret('secret/infra/selectel/terraform', 'SEL_TOKEN', 'terraform')}",
+        //   "TF_VAR_DNS_KEY_SECRET=${utils.getVaultSecret('secret/infra/selectel/terraform', 'TF_VAR_DNS_KEY_SECRET', 'terraform')}",
+        //   "TF_VAR_OPENSTACK_PASSWORD=${utils.getVaultSecret('secret/infra/selectel/terraform', 'TF_VAR_OPENSTACK_PASSWORD', 'terraform')}",
+        //   "TF_VAR_OPENSTACK_USERNAME=${utils.getVaultSecret('secret/infra/selectel/terraform', 'TF_VAR_OPENSTACK_USERNAME', 'terraform')}",
+        //   // "TF_PLUGIN_CACHE_DIR=\$HOME/.terraform.d/"
+        // ]) {
+        //   templateToProcessPaths.each { file ->
+        //     if (fileExists(file)) {
+        //       sh """
+        //         cd ${file.minus('terragrunt.hcl')}
+        //         terragrunt init
+        //         terragrunt plan              
+        //       """
+        //       if (env.GIT_BRANCH == 'develop') {
+        //         sh """
+        //           cd ${file.minus('terragrunt.hcl')}
+        //           terragrunt apply -auto-approve
+        //         """
+        //       }
+        //     }
+        //   }
+        // }
       }
+    }
+  }
+}
+
+@NonCPS
+List getChangedTemplateFiles() {
+  changedFiles = []
+  println currentBuild.changeSets
+  for (changeLogSet in currentBuild.changeSets) { 
+    for (entry in changeLogSet.getItems()) {
+      for (file in entry.getAffectedFiles()) {
+        String filePath = file.getPath()
+        if (filePath.startsWith('environments/project-a') && filePath.endsWith('terragrunt.hcl')) {
+          changedFiles.add(file.getPath())
+        }
+      }
+    }
   }
   return changedFiles
 }
 
-println getChangedFilesList()
-
-// def call(Map inputPipelineParams) {
-//   pipelineParams = inputPipelineParams
+// String getPackerKeys(scmVars) {
+//   if (env.GIT_BRANCH == "master") {
+//     flavor = "fd9aad45-220d-43a4-94d8-013f1021dfe8"
+//     image_name_postfix = ""
+//     networks = "faeaec0c-2dc7-4b30-8f40-3ca6838160cd"
+//     tenant_id = "f170527a070e458ca71b1e7bfc882000"
+//   } else {
+//     flavor = "faf056f7-128e-43b5-a43b-372debdf71b6"
+//     image_name_postfix = "-${scmVars.GIT_BRANCH}-${scmVars.GIT_COMMIT[0..7]}"
+//     networks = "80d6604f-3425-49ff-a76a-c3ebf43bca83"
+//     tenant_id = "79ba7bbe72d2441a8d2c7da52587fd96"
+//   }
   
-//   properties([
-//     buildDiscarder(logRotator(numToKeepStr: getNumberOfBuildsToStore())),
-//     disableConcurrentBuilds(),
-//     parameters(getParamsList())
-//   ])
-
-//   pipeline {
-//     agent {
-//       kubernetes {
-//         label "${getJobName()}-${currentBuild.timeInMillis}"
-//         defaultContainer "jnlp"
-//         yaml libraryResource("jenkins-agents/${getAgentContainerName()}.yaml")
-//       }
-//     }
-
-//     stages {
-//       stage("Build with Packer") {
-//         steps {
-//           container(name: "${getAgentContainerName()}", shell: "/bin/bash") {
-//             script {
-              
-
-
-//               sh """
-//                 packer validate ${getPackerTemplate()} 
-//               """
-//               if (params.DEPLOY) {
-//                 sh """
-//                   packer build ${getPackerTemplatePath()} ${getPackerAdditionalData()}
-//                 """
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
-
-// @groovy.transform.Field
-// defaultParamsList = [
-//   booleanParam(
-//     name: 'DEPLOY',
-//     defaultValue: false,
-//     description: 'Deploy (dry-run by default)'),
-//   string(
-//     name: 'PACKER_OPTIONAL_DATA',
-//     defaultValue: "",
-//     description: 'Optional input data for helm upgrade command', )
-// ]
-
-// String getAgentContainerName() {
-//   return pipelineParams.agentContainerName ?: 'packer'
-// }
-
-// String getJobName() {
-//   String srcJobName = env.JOB_NAME
-//   return srcJobName.replaceAll('/','-')
-// }
-// String getPackerTemplatePath() {
-//   return pipelineParams.packerTemplatePath ?: 'packer.json'
-// }
-
-// String getPackerAdditionalData() {
-//   return params.PACKER_OPTIONAL_DATA ?: (pipelineParams.packerAdditionalData ?: '')
-// }
-
-// String getNumberOfBuildsToStore() {
-//   return pipelineParams.numberOfBuildsToStore ?: '3'
-// }
-
-// String getParamsList() {
-//   def additionalParamsNames = pipelineParams.additionalParams.collect { 
-//     param -> return param.getArguments().get('name') 
-//   }
-//   def defaultParamsListFiltered = defaultParamsList.findAll { 
-//     param -> !(additionalParamsNames.contains(param.getArguments().get('name'))) 
-//   }
-//   return defaultParamsListFiltered + pipelineParams.additionalParams
+//   availability_zone = "ru-1a"
+//   region = "ru-1"
+//   volume_type = "basic.ru-1a"  
+  
+//   return "-var 'flavor=${flavor}' \
+//     -var 'image_name_postfix=${image_name_postfix}' \
+//     -var 'networks=${networks}' \
+//     -var 'tenant_id=${tenant_id}' \
+//     -var 'availability_zone=${availability_zone}' \
+//     -var 'region=${region}' \
+//     -var 'volume_type=${volume_type}'"
 // }
