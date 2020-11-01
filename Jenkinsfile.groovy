04c3f9c4e32cc7f400115c1dfd45dac4b889dc7e
Ilya Solovyev <ilya.solovyev@hotmail.com>
2020-11-01 22:32:20 +0300

    WIP - import onef-prod vms

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,10 +11,19 @@ podTemplate(yaml: libraryResource("jenkins-agents/terraform.yaml")) {
     stage('TEST GIT CHANGES') {
       container(name:'terraform', shell: "/bin/bash") {
         def scmVars = checkout(scm)
-        sh """
-          git diff --name-only HEAD^ HEAD
-          git diff --name-only --diff-filter=D HEAD^ HEAD
-        """
+        def filesDeleted = "git diff --name-only --diff-filter=D HEAD^ HEAD"
+        def filesCreatedOrUpdated = "git diff --name-only --diff-filter=ACMR HEAD^ HEAD"
+        def filesDeletedResult = sh( returnStdout: true, script: filesDeleted )
+        def filesCreatedOrUpdatedResult = sh( returnStdout: true, script: filesCreatedOrUpdated )
+        println "${filesDeletedResult}"
+        println "${filesCreatedOrUpdatedResult}"
+        // if( errors.trim() ){
+        //     println "errors found: ${errors}"
+        // }
+        // sh """
+        //   git diff --name-only HEAD^ HEAD
+          
+        // """
       }
     }
   }
